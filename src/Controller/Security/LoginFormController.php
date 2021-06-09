657bb2e86258ea9bd36143e0188c290e49630a32
Nikolai Plath <der.el.kuku@gmail.com>
2021-06-09 09:04:48 -0500

    Move security controllers

--- src/Controller/Security/LoginFormController.php
+++ src/Controller/Security/LoginFormController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class LoginFormController extends AbstractController
 {
     #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
     public function login(
