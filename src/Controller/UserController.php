60cfe224cacc5c2e5cd037ba27b6f016ea52cc19
Nikolai Plath <der.el.kuku@gmail.com>
2021-11-30 10:31:25 -0500

    Don't use getDoctrine(9 in controllers anymore...

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,14 +35,14 @@ class UserController extends AbstractController
 
     #[Route('/new', name: 'user_new', methods: ['GET', 'POST'])]
     public function new(
-        Request $request
+        Request $request,
+        EntityManagerInterface $entityManager,
     ): Response {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -76,13 +77,14 @@ class UserController extends AbstractController
     #[Route('/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
     public function edit(
         Request $request,
-        User $user
+        User $user,
+        EntityManagerInterface $entityManager,
     ): Response {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager->flush();
 
             return $this->redirectToRoute('user_index');
         }
@@ -103,14 +105,14 @@ class UserController extends AbstractController
     #[Route('/delete/{id}', name: 'user_delete', methods: ['POST'])]
     public function delete(
         Request $request,
-        User $user
+        User $user,
+        EntityManagerInterface $entityManager,
     ): Response {
         if ($this->isCsrfTokenValid(
             'delete'.$user->getId(),
             $request->request->get('_token')
         )
         ) {
-            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($user);
             $entityManager->flush();
         }
