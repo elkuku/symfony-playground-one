5d35916679df5695b7713b27e1613b487c13fde8
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-26 06:53:50 -0500

    Remove unused imports

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
