a1eaae22f403cc3161e00212413b4353d281f06b
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-27 20:15:13 -0500

    Add forgotten bootstrap js :P

--- assets/app.js
+++ assets/app.js
@@ -12,5 +12,6 @@ import './styles/app.css';
 import './bootstrap';
 
 import '@popperjs/core'
+import 'bootstrap';
 import 'bootswatch/dist/cosmo/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
