b8e0237349b85758ea0733b39a5aa887af08de63
Nikolai Plath <der.el.kuku@gmail.com>
2022-05-17 11:51:27 -0500

    Remove dama/doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
