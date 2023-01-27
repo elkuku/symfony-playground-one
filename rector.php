f6966951a50149f6f3fd9d0c3bf25ccbcbbb4fe0
Nikolai Plath <der.el.kuku@gmail.com>
2023-01-27 05:30:43 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -15,7 +15,7 @@ return static function (RectorConfig $rectorConfig): void {
     );
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
 
         SymfonyLevelSetList::UP_TO_SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
